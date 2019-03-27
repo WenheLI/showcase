5f22814e676cb52754d6c69ceaef4d66e61880e8
WenheLi <wl1508@nyu.edu>
2019-03-27 11:17:30 -0400

    add translate impl

--- asg6/Matrix.js
+++ asg6/Matrix.js
@@ -1,29 +1,62 @@
 class Matrix {
-    constructor(input) {
-        this.value = input; // [[], [], [], []]
+    constructor() {
+        this.IDENTITY = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+        this.stacks = [this.IDENTITY];
+        this.sp = 0;
     }
 
-    add(matrix) {
-        if(matrix instanceof Matrix) return this.value.map((vec, i) => vec.map((scala, j) => scala + matrix[i][j]));
-        else if (typeof matrix === 'number') return this.value.map((vec) => vec.map((scale) => scale + matrix));
+    translate(...numOrArr) {
+        const tempMatrix = this.__deepCopy(this.IDENTITY);
+        
+        if (numOrArr.length === 1) numOrArr = numOrArr[0];
+
+        if (Array.isArray(numOrArr) && numOrArr.length === 3) tempMatrix[3] = [...numOrArr, 1];
+        else if (typeof numOrArr === 'number') tempMatrix[3] = [numOrArr, numOrArr, numOrArr, 1];
         else throw new Error("Should only be matrix or number type");
+        this.__mul(tempMatrix);
+        return this.stacks[this.sp];
     }
 
-    sub(matrix) {
-        if(matrix instanceof Matrix) return this.value.map((vec, i) => vec.map((scala, j) => scala - matrix[i][j]));
-        else if (typeof matrix === 'number') return this.value.map((vec) => vec.map((scale) => scale + matrix));
+
+    __deepCopy(matrix) {
+        const temp = [];
+        matrix.forEach((it) => temp.push(it.concat()));
+        return temp;
+    }
+
+    __add(matrix) {
+        let value = this.stacks[this.sp];
+        if( Array.isArray(matrix) && matrix.length == 4 ) value = value.map((vec, i) => vec.map((scala, j) => scala + matrix[i][j]));
+        else if (typeof matrix === 'number') value = value.map((vec) => vec.map((scale) => scale + matrix));
         else throw new Error("Should only be matrix or number type");
+        this.stacks[this.sp] = value;
+        return value;
     }
 
-    div(num) {
-        if (typeof num === 'number') return this.value.map((vec) => vec.map((scale) => scale / num));
+    __sub(matrix) {
+        let value = this.stacks[this.sp];
+        if(Array.isArray(matrix) && matrix.length == 4) value = value.map((vec, i) => vec.map((scala, j) => scala - matrix[i][j]));
+        else if (typeof matrix === 'number') value = value.map((vec) => vec.map((scale) => scale - matrix));
+        else throw new Error("Should only be matrix or number type");
+        this.stacks[this.sp] = value;
+        return value;
+    }
+
+    __div(num) {
+        let value = this.stacks[this.sp];
+        if (typeof num === 'number') value = value.map((vec) => vec.map((scale) => scale / num));
         else throw new Error("Should only be number type");
+        this.stacks[this.sp] = value;
+        return value;
     }
 
-    mul(matrix) {
-        if (typeof num === 'number') return this.value.map((vec) => vec.map((scale) => scale * num));
-        else if (matrix instanceof Matrix) return this.value.map((vec, i) => vec.map((scala, j) => scala * matrix[j][i]));
+    __mul(matrix) {
+        let value = this.stacks[this.sp];
+        if (typeof matrix === 'number') value = value.map((vec) => vec.map((scale) => scale * matrix));
+        else if (Array.isArray(matrix) && matrix.length == 4) value = value.map((vec) => vec.map((scala, i) => vec.reduce((sum, curr, j) => sum + curr*matrix[j][i], 0)));
         else throw new Error("Should only be matrix or number type");
+        this.stacks[this.sp] = value;
+        return value;
     }
 }
 
